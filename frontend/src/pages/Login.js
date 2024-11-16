29c16ac5998f113fd9203b32130470ad84e2d614
Y3s3h <yy6922341@gmail.com>
2024-11-16 13:44:19 +0530

    routes removed

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -153,16 +153,13 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch(
-        "https://e-watse-locator.vercel.app/api/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email, password }),
-        }
-      );
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
 
       const data = await response.json();
       if (response.ok) {
